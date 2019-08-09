584638af6ac1c28f19efe265bbd28ad124cd0427
이성수 <tjdtjdtn1214@naver.com>
2019-08-09 22:53:18 +0900

    what the error

--- 3.숫자야구/NumberBaseball.vue
+++ 3.숫자야구/NumberBaseball.vue
@@ -3,9 +3,12 @@
         <h1>{{result}}</h1>
         <form v-on:submit="onSubmitForm">
             <input ref="answer" maxlength="4" v-model="value" />
-            <button>입력</button>
+            <button type="submit">입력</button>
         </form>
-        <div>시도: {{}}</div>
+        <div>시도: {{tries.length}}</div> <!-- tries.length: 배열의 총 개수 -->
+        <ul>
+            <li v-for="t in tries">{{t}}</li> <!-- tries: 배열 -->
+        </ul>
     </div>
 </template>
 
@@ -13,6 +16,7 @@
     export default { //전역 컴포넌트
         data() {
             return{
+                tries: [], //[]: 배열
                 value: '',
                 result: '',
 
@@ -21,6 +25,10 @@
         methods: {
             onSubmitForm(e) {
                 e.preventDefault();
+                console.log(t);
+                this.tries.push(this.value); //입력
+                this.value = '';
+                this.$refs.answer.focus();
             }
 
         }
@@ -32,3 +40,4 @@
 </style>
 
 <!-- 특수한 문법을 쓰는 js파일. es6문법 -->
+<!-- 이 파일은 vue-loader가 처리한다. -->
